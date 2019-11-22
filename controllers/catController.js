d6bc60a8122bd7b841deba2495ee52fc17d0bfb4
hexarules <hexarules@gmail.com>
2019-11-22 21:02:24 +0200

    4tehtävää tehty

--- controllers/catController.js
+++ controllers/catController.js
@@ -1,2 +1,29 @@
 'use strict';
 // catController
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+app.get('/cat', (req, res) => {
+    res.send('With this endpoint you can get cats.');
+});
+app.get('/cat/:id/', (req, res) => {
+    res.send(req.params);
+});
+
+
+
+app.post('/cat', (req, res) => {
+    res.send('With this endpoint you can add cats.');
+});
+app.put('/cat', (req, res) => {
+    res.send('With this endpoint you can edit cats.');
+});
+app.delete('/cat', (req, res) => {
+    res.send('With this endpoint you can delete cats.');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
